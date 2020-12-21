ccace56bf2c3de2f47747b499fb08a08460ea636
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2020-12-21 16:13:25 +0900

    Add lectures watched in part

--- src/feature/add_lectures_watched_in_part.py
+++ src/feature/add_lectures_watched_in_part.py
@@ -18,9 +18,20 @@ if __name__ == '__main__':
         train_df.drop(columns=['lecture_id'], inplace=True)
         print(train_df.head())
 
-        lectures_watched_in_part = train_df[train_df['content_type_id'] == 1].groupby(['user_id', 'part'])['row_id'].nunique()
+        lectures_watched_in_part = train_df[train_df['content_type_id'] == 1].groupby(['user_id', 'part'])['row_id']\
+            .nunique()
         lectures_watched_in_part = pd.DataFrame(lectures_watched_in_part)
         lectures_watched_in_part = lectures_watched_in_part.reset_index()
         lectures_watched_in_part = lectures_watched_in_part.rename(columns={'row_id': 'lectures_watched_in_part'})
         print(lectures_watched_in_part.head())
         lectures_watched_in_part.to_csv("data/processed/lectures_watched_in_part.csv")
+
+        train_with_bundle_id_part_df = pd.read_csv("data/interim/train_with_bundle_id_part.csv")
+        train_with_lecture_count = train_with_bundle_id_part_df.merge(lectures_watched_in_part,
+                                                                      on=['user_id', 'part'],
+                                                                      how='left')
+
+        train_with_lecture_count['lectures_watched_in_part'] = train_with_lecture_count['lectures_watched_in_part']\
+            .fillna(0)
+        print(train_with_lecture_count.head())
+        train_with_lecture_count.to_csv("data/interim/train_with_lecture_count.csv")
