e5b744436bbbb4272f3ab6f123f05240bf607d4e
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2021-01-25 23:28:16 +0900

    few new features

--- src/feature/add_features.py
+++ src/feature/add_features.py
@@ -16,5 +16,15 @@ if __name__ == '__main__':
         train[additional_features[0]] = train['cont13_logarithm'] * train['cont4']
         test[additional_features[0]] = test['cont13_logarithm'] * test['cont4']
 
+        train[additional_features[1]] = train['cont1'] - train['cont12']
+        test[additional_features[1]] = test['cont1'] - test['cont12']
+
+        train[additional_features[2]] = train['cont12'] / train['cont13_logarithm']
+        test[additional_features[2]] = test['cont12'] / test['cont13_logarithm']
+
+        train[additional_features[3]] = train['cont12'] + train['cont3']
+        test[additional_features[3]] = test['cont12'] + test['cont3']
+
+
         train.to_csv("data/interim/train_add_features.csv", index=False)
         test.to_csv("data/interim/test_add_features.csv", index=False)
